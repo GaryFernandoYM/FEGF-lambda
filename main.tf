54e9d2a239664f0b6b57297b8649bad3d2c4e1b6
GaFer <89945798+GaryFernandoYM@users.noreply.github.com>
2025-06-24 15:43:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,59 +4,36 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
+# Reutilizar IAM Role ya creado
+data "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role_gary_yunganina_v3"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
 }
 
-resource "aws_iam_policy" "lambda_s3_policy" {
+# Reutilizar IAM Policy ya creada
+data "aws_iam_policy" "lambda_s3_policy" {
   name = "lambda_s3_read_policy_v3"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "s3:ListBucket",
-          "s3:GetObject"
-        ],
-        Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
-        ]
-      }
-    ]
-  })
 }
 
+# Adjuntar la política básica de logs
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec_role.name
+  role       = data.aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Adjuntar la política personalizada al rol
 resource "aws_iam_policy_attachment" "lambda_s3_policy_attach" {
   name       = "lambda_s3_policy_attach_v2"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+  roles      = [data.aws_iam_role.lambda_exec_role.name]
+  policy_arn = data.aws_iam_policy.lambda_s3_policy.arn
 }
 
+# Crear la función Lambda con los recursos existentes
 resource "aws_lambda_function" "my_lambda" {
-  function_name = var.function_name
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  filename      = "function.zip"
+  function_name    = var.function_name
+  role             = data.aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  filename         = "function.zip"
   source_code_hash = filebase64sha256("function.zip")
 
   environment {
