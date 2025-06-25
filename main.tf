5938df48394236ea056afa58ac3db90b5dfb5372
GaFer <89945798+GaryFernandoYM@users.noreply.github.com>
2025-06-25 18:17:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,30 +4,25 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-# Reutilizar IAM Role ya creado
 data "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role_gary_yunganina_v3"
 }
 
-# Reutilizar IAM Policy ya creada
 data "aws_iam_policy" "lambda_s3_policy" {
   name = "lambda_s3_read_policy_v3"
 }
 
-# Adjuntar la política básica de logs
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = data.aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Adjuntar la política personalizada al rol
 resource "aws_iam_policy_attachment" "lambda_s3_policy_attach" {
   name       = "lambda_s3_policy_attach_v2"
   roles      = [data.aws_iam_role.lambda_exec_role.name]
   policy_arn = data.aws_iam_policy.lambda_s3_policy.arn
 }
 
-# Crear la función Lambda con los recursos existentes
 resource "aws_lambda_function" "my_lambda" {
   function_name    = var.function_name
   role             = data.aws_iam_role.lambda_exec_role.arn
