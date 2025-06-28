c3f473b4c8e0b0cb188b3f6cae341a014d0ecb87
GaFer <89945798+GaryFernandoYM@users.noreply.github.com>
2025-06-28 11:26:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,30 +4,38 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-data "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role_gary_yunganina_v3"
+
+locals {
+  existing_lambda_role_arn = "arn:aws:iam::864981734585:role/lambda_exec_role_yes"
 }
+
 
 data "aws_iam_policy" "lambda_s3_policy" {
-  name = "lambda_s3_read_policy_v3"
+  name = "lambda_s3_read_policy_yes"
 }
 
+
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = data.aws_iam_role.lambda_exec_role.name
+  role       = "lambda_exec_role_yes"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+
 resource "aws_iam_policy_attachment" "lambda_s3_policy_attach" {
-  name       = "lambda_s3_policy_attach_v2"
-  roles      = [data.aws_iam_role.lambda_exec_role.name]
+  name       = "lambda_s3_policy_attach_yes"
+  roles      = ["lambda_exec_role_yes"]
   policy_arn = data.aws_iam_policy.lambda_s3_policy.arn
 }
 
+
 resource "aws_lambda_function" "my_lambda" {
-  function_name    = var.function_name
-  role             = data.aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
+  function_name = var.function_name
+  role          = local.existing_lambda_role_arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 10
+  memory_size   = 256
+
   filename         = "function.zip"
   source_code_hash = filebase64sha256("function.zip")
 
